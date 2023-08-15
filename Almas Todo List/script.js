27e99726dd6ba5bf8ce1f97b4dd0a6de4e51fe17
Alma Duffey <1984ahern@gmail.com>
2023-08-15 12:04:59 -0500

    get request works

---
+++ Almas Todo List/script.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+  $.ajax({
+    type: 'GET',
+    url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=1',
+    dataType: 'json',
+    success: function (response, textStatus) {
+        response.tasks.forEach(function (task) {
+            $('#todo-list').append('<p>' + task.content + '</p>');
+          })
+        },
+        error: function (request, textStatus, errorMessage) {
+          console.log(errorMessage);
+        }
+      });
+    });
+
+    var createTask = function () {
+    $.ajax({
+        type: 'POST',
+        url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=1',
+        contentType: 'application/json',
+        dataType: 'json',
+        data: JSON.stringify({
+          task: {
+            content: $('#new-task-content').val()
+          }
+        }),
+        success: function (response, textStatus) {
+          console.log(response);
+        },
+        error: function (request, textStatus, errorMessage) {
+          console.log(errorMessage);
+        }
+      });
+      };
+
+      $('#create-task').on('submit', function (e) {
+        e.preventDefault();
+        createTask();
+      });
