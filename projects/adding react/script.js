55799433a048f7fc4b136113d6b90e35d2585a44
Alma Duffey <1984ahern@gmail.com>
2023-08-19 12:16:57 -0500

    adding multiple elements

--- projects/adding react/script.js
+++ projects/adding react/script.js
@@ -8,6 +8,29 @@ const element = React.createElement(
     'Hello World!'
 );
 
+const newElement = React.createElement(
+    'h3',
+    {
+        className: 'paragraphs',
+        onClick: () => { console.log('clicked');},
+        style: { color: 'blue'}
+    },
+        'this is a paragraphs blah blah blah'
+);
+
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 root.render(element);
+
+const container2 = document.getElementById('root2');
+const root2 = ReactDOM.createRoot(container2);
+root2.render(element);
+
+const container3 = document.getElementById('practice');
+const practice = ReactDOM.createRoot(container3);
+practice.render(newElement);
+
+const container4 = document.getElementById('practice2');
+const practice2 = ReactDOM.createRoot(container4);
+practice2.render(newElement);
+
