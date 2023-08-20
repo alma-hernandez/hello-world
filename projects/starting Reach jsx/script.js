48e054da7ef7d83447172f83b5d62c9579e9aaf0
Alma Duffey <1984ahern@gmail.com>
2023-08-19 21:40:09 -0500

    changed color with classnames

--- projects/starting Reach jsx/script.js
+++ projects/starting Reach jsx/script.js
@@ -1,6 +1,34 @@
-const name = 'John';
-const element = <h1>Hello {name}</h1>;
-
-const container = document.getElementById('root');
-const root = ReactDOM.createRoot(container);
-root.render(element);
+const formulateGreeting = (user) => {
+    if (user) {
+      let greeting;
+      switch (user.language) {
+        case 'es':
+          greeting = 'Hola';
+          break;
+        case 'de':
+          greeting = 'Hallo';
+          break;
+        default:
+          greeting = 'Hello';
+      }
+      return (
+        <h1 className={`greeting ${user.language}`}>
+          {`${greeting} ${user.firstName} ${user.lastName}`}
+        </h1>
+      );
+    }
+  
+    return <h1 className="greeting">Hello my friend</h1>;
+  };
+  
+  const user = {
+    language: 'es',
+    firstName: 'Javier',
+    lastName: 'Bandela'
+  };
+  
+  const element = formulateGreeting(user);
+  
+  const container = document.getElementById('root');
+  const root = ReactDOM.createRoot(container);
+  root.render(element);
